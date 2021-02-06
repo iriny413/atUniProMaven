411775be1f719ba1f68f2660525e7bf6751a5903
ikhvichi <irina.khvichiya@t-systems.com>
2021-02-06 21:34:47 +0100

    added a class for just adhoc tests

---
+++ src/main/java/atUniProMaven/AdhocTests.java
@@ -0,0 +1,12 @@
+package atUniProMaven;
+
+public class AdhocTests {
+    public static void main(String[] args) {
+        String str = "aa bB Ac cD da";
+        String str1 = str.replace('a', 'r');
+        String str2 = str.replace("a", "r");
+        System.out.println(str);
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
